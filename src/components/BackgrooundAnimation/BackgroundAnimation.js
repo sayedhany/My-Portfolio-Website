7074c7693d755e79cc85ac1dc3bca2d74a6e2966
sayedhany <sayedh@mail.ru>
2023-03-09 21:32:48 +0200

    fixed

--- src/components/BackgrooundAnimation/BackgroundAnimation.js
+++ src/components/BackgrooundAnimation/BackgroundAnimation.js
@@ -6,7 +6,7 @@ const BackgroundAnimation = () => (
       className="BgAnimation__svg"
       viewBox="0 0 602 602"
       fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+         xmlns="http://www.w3.org/2000/svg"
     >
       <g opacity="0.15">
         <path
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,10 @@ const Home = () => {
   return (
     <>
       <Head>
+        <meta
+          name="description"
+          content="I'm Sayed Hany, I'm 23 years and I'm from Cairo, I graduated from Menofia University, faculty of science and Pure mathematics and computer science department, and creative and self-starting Entry level Front-End Developer. Building stable websites and apps in fast-paced, collaborative environments. Implementation of apps and landing pages from concept through deployment. Maintaining, optimizing, troubleshooting, and improving websites."
+        ></meta>
         <title>Sayed Hany</title>
       </Head>
       <Layout>
